a9836e6282958c0faefe4bf36973a3776cd38632
Sergei Chipiga <chipiga86@gmail.com>
2017-12-15 22:58:57 +0200

    expand tests

--- tests/unit/testCommands.js
+++ tests/unit/testCommands.js
@@ -39,6 +39,67 @@ scope("commands", () => {
         });
     });
 
+    test(".launchHttpProxy()", () => {
+
+        beforeChunk(() => {
+            CONF.web.url = "https://yandex.ru";
+            cmd._isHttpProxyLaunched = sinon.stub().returns(false);
+            cmd._httpProxy = null;
+        });
+
+        chunk("skipped if http proxy already launched", async () => {
+            cmd._isHttpProxyLaunched = sinon.stub().returns(true);
+            expect(await cmd.launchHttpProxy()).to.be.false;
+        });
+
+        chunk("skipped if proxied url isn't defined", async () => {
+            CONF.web.url = null;
+            expect(await cmd.launchHttpProxy()).to.be.false;
+        });
+
+        chunk("starts new http proxy instance", async () => {
+            var proxyOpts;
+
+            CONF.proxy.port = 3000;
+            CONF.proxy.timeout = 1000;
+            CONF.proxy.reconnect = 10;
+            CONF.cache.use = true;
+            CONF.proxy.speed = 128;
+
+            cmd.__HttpProxy = function (opts) {
+                proxyOpts = opts;
+                this.start = () => Promise.resolve();
+            };
+
+            expect(await cmd.launchHttpProxy()).to.be.true;
+            expect(proxyOpts.port).to.be.equal(3000);
+            expect(proxyOpts.timeout).to.be.equal(1000);
+            expect(proxyOpts.reconnect).to.be.equal(10);
+            expect(proxyOpts.useCache).to.be.true;
+            expect(proxyOpts.url).to.be.equal("https://yandex.ru");
+            expect(proxyOpts.speed).to.be.equal(128);
+        });
+
+        chunk("starts existing http proxy instance", async () => {
+            cmd._httpProxy = {
+                start: () => Promise.resolve(),
+            };
+
+            expect(await cmd.launchHttpProxy()).to.be.true;
+        });
+
+        chunk("restarts chrome browser", async () => {
+            cmd.__HttpProxy = function () {
+                this.start = () => Promise.resolve();
+            };
+            cmd._isChromeLaunched = () => true;
+            cmd.restartChrome = sinon.spy();
+
+            expect(await cmd.launchHttpProxy()).to.be.true;
+            expect(cmd.restartChrome.calledOnce).to.be.true;
+        });
+    });
+
     test(".stopHttpProxy()", () => {
 
         chunk("stops http proxy", async () => {
@@ -55,6 +116,76 @@ scope("commands", () => {
         });
     });
 
+    test(".restartHttpProxy()", () => {
+
+        chunk("restarts http proxy", async () => {
+            cmd.stopHttpProxy = sinon.stub().returns(Promise.resolve());
+            cmd.launchHttpProxy = sinon.stub().returns(Promise.resolve());
+            await cmd.restartHttpProxy();
+
+            expect(cmd.stopHttpProxy.calledOnce).to.be.true;
+            expect(cmd.launchHttpProxy.calledOnce).to.be.true;
+        });
+    });
+
+    test(".launchGlobalProxy()", () => {
+
+        beforeChunk(() => {
+            cmd._isGlobalProxyLaunched = sinon.stub().returns(false);
+            cmd._globalProxy = null;
+        });
+
+        chunk("skipped if global proxy already launched", async () => {
+            cmd._isGlobalProxyLaunched = sinon.stub().returns(true);
+            expect(await cmd.launchGlobalProxy()).to.be.false;
+        });
+
+        chunk("starts new global proxy instance", async () => {
+            var proxyOpts;
+
+            CONF.proxy.globalPort = 3000;
+            CONF.proxy.timeout = 1000;
+            CONF.proxy.reconnect = 10;
+            CONF.proxy.installCertificate = true;
+            CONF.proxy.sslCaDir = ".";
+            CONF.cache.use = true;
+            CONF.proxy.speed = 128;
+
+            cmd.__GlobalProxy = function (opts) {
+                proxyOpts = opts;
+                this.start = () => Promise.resolve();
+            };
+
+            expect(await cmd.launchGlobalProxy()).to.be.true;
+            expect(proxyOpts.port).to.be.equal(3000);
+            expect(proxyOpts.timeout).to.be.equal(1000);
+            expect(proxyOpts.reconnect).to.be.equal(10);
+            expect(proxyOpts.installCertificate).to.be.true;
+            expect(proxyOpts.sslCaDir).to.be.equal(".");
+            expect(proxyOpts.useCache).to.be.true;
+            expect(proxyOpts.speed).to.be.equal(128);
+        });
+
+        chunk("starts existing global proxy instance", async () => {
+            cmd._globalProxy = {
+                start: () => Promise.resolve(),
+            };
+
+            expect(await cmd.launchGlobalProxy()).to.be.true;
+        });
+
+        chunk("restarts chrome browser", async () => {
+            cmd.__GlobalProxy = function () {
+                this.start = () => Promise.resolve();
+            };
+            cmd._isChromeLaunched = () => true;
+            cmd.restartChrome = sinon.spy();
+
+            expect(await cmd.launchGlobalProxy()).to.be.true;
+            expect(cmd.restartChrome.calledOnce).to.be.true;
+        });
+    });
+
     test(".stopGlobalProxy()", () => {
 
         chunk("stops global proxy", async () => {
@@ -68,6 +199,18 @@ scope("commands", () => {
 
         chunk("is skipped if global proxy isn't launched", async () => {
             expect(await cmd.stopGlobalProxy()).to.be.false;
+        });
+    });
+
+    test(".restartGlobalProxy()", () => {
+
+        chunk("restarts global proxy", async () => {
+            cmd.stopGlobalProxy = sinon.stub().returns(Promise.resolve());
+            cmd.launchGlobalProxy = sinon.stub().returns(Promise.resolve());
+            await cmd.restartGlobalProxy();
+
+            expect(cmd.stopGlobalProxy.calledOnce).to.be.true;
+            expect(cmd.launchGlobalProxy.calledOnce).to.be.true;
         });
     });
 
---
+++ tests/unit/testPluginHelp.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var pluginHelp = require("../../lib").pluginHelp;
+
+test("plugin help contains option", () => {
+
+    var opts = pluginHelp({ options: opts => opts }, d => d);
+
+    chunk("http-proxy", () => {
+        expect(opts["http-proxy"]).to.exist;
+    });
+
+    chunk("http-proxy-port", () => {
+        expect(opts["http-proxy-port"]).to.exist;
+    });
+
+    chunk("global-proxy", () => {
+        expect(opts["global-proxy"]).to.exist;
+    });
+
+    chunk("global-proxy-port", () => {
+        expect(opts["global-proxy-port"]).to.exist;
+    });
+
+    chunk("install-certificate", () => {
+        expect(opts["install-certificate"]).to.exist;
+    });
+
+    chunk("cache", () => {
+        expect(opts["cache"]).to.exist;
+    });
+
+    chunk("existing-cache", () => {
+        expect(opts["existing-cache"]).to.exist;
+    });
+
+    chunk("reconnect", () => {
+        expect(opts["reconnect"]).to.exist;
+    });
+});
