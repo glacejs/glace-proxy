397784193cdd151ddd97cd8e175c12dd11d64a34
Sergei Chipiga <chipiga86@gmail.com>
2018-03-30 12:56:18 +0300

    remove redundant step check

--- tests/e2e.conftest.js
+++ tests/e2e.conftest.js
@@ -36,77 +36,62 @@ global.test = (name, opts, fixtures, func) => {
 glace.Steps.register({
 
     launch_chrome: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.launchChrome()).to.be.true;
     },
 
     close_chrome: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.closeChrome()).to.be.true;
     },
     
     restart_chrome: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.restartChrome()).to.be.true;
     },
 
     launch_global_proxy: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.launchGlobalProxy()).to.be.true;
     },
 
     stop_global_proxy: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.stopGlobalProxy()).to.be.true;
     },
 
     restart_global_proxy: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.restartGlobalProxy()).to.be.true;
     },
 
     launch_http_proxy: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.launchHttpProxy()).to.be.true;
     },
 
     stop_http_proxy: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.stopHttpProxy()).to.be.true;
     },
 
     restart_http_proxy: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.restartHttpProxy()).to.be.true;
     },
 
     set_proxied_url: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.setProxiedUrl("https://github.com")).to.be.true;
     },
 
     set_proxy_speed: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.setProxySpeed(32)).to.be.true;
     },
 
     reset_proxy_speed: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.resetProxySpeed()).to.be.true;
     },
 
     enable_proxy_cache: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.enableProxyCache()).to.be.true;
     },
 
     disable_proxy_cache: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.disableProxyCache()).to.be.true;
     },
 
     clear_proxy_cache: async function () {
-        if (this.isTestFailed()) return false;
         expect(await cmd.clearProxyCache()).to.be.true;
     },
 });
